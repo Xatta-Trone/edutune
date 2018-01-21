df06a7216237950d1fcef5b35fc6603382ab7099
Monzurul Islam <monzurul.ce.buet@gmail.com>
2018-01-21 23:08:23 +0600

    all done

--- js/main.js
+++ js/main.js
@@ -6,35 +6,6 @@
         	type: 'video'
         });
 
-        $('#map')
-      .gmap3({
-        center:[41.850033, -87.650052],
-        zoom:5,
-        mapTypeId: "shadeOfGrey", // to select it directly
-        mapTypeControlOptions: {
-          mapTypeIds: [google.maps.MapTypeId.ROADMAP, "shadeOfGrey"]
-        },
-        scrollwheel: false,
-      })
-      .styledmaptype(
-        "shadeOfGrey",
-        [
-          {"featureType":"all","elementType":"labels.text.fill","stylers":[{"saturation":36},{"color":"#9b8a8b"},{"lightness":40}]},
-          {"featureType":"all","elementType":"labels.icon","stylers":[{"visibility":"off"}]},
-          {"featureType":"administrative","elementType":"geometry.fill","stylers":[{"color":"#fff"},{"lightness":20}]},
-          {"featureType":"administrative","elementType":"geometry.stroke","stylers":[{"color":"#fff"},{"lightness":17},{"weight":1.2}]},
-          {"featureType":"landscape","elementType":"geometry","stylers":[{"color":"#fff"},{"lightness":20}]},
-          {"featureType":"poi","elementType":"geometry","stylers":[{"color":"#fff"},{"lightness":21}]},
-          {"featureType":"road.highway","elementType":"geometry.fill","stylers":[{"color":"#f3f3f3"},{"lightness":17}]},
-          {"featureType":"road.highway","elementType":"geometry.stroke","stylers":[{"color":"#f3f3f3"},{"lightness":29},{"weight":0.2}]},
-          {"featureType":"road.arterial","elementType":"geometry","stylers":[{"color":"#f3f3f3"},{"lightness":18}]},
-          {"featureType":"road.local","elementType":"geometry","stylers":[{"color":"#f3f3f3"},{"lightness":16}]},
-          {"featureType":"transit","elementType":"geometry","stylers":[{"color":"#f3f3f3"},{"lightness":19}]},
-          {"featureType":"water","elementType":"geometry","stylers":[{"color":"#c8d7d4"},{"lightness":17}]}
-        ],
-        {name: "Shades of Grey"}
-      );
-
 
 
 
